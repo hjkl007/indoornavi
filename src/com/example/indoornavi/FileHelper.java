291b2c6743d7fc4bc0f912cb599d04fc1036ad7d
zhr01 <1210984456@qq.com>
2016-01-09 11:28:16 +0800

    the first version that can complish all task.

---
+++ src/com/example/indoornavi/FileHelper.java
@@ -0,0 +1,95 @@
+package com.example.indoornavi;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.annotation.SuppressLint;
+import android.os.Environment;
+import android.util.Log;
+
+/**
+ * Created by doom on 15/4/2.
+ */
+public class FileHelper {
+	
+	public static String getContent(String fileName) {
+		try {
+
+			InputStreamReader inputReader = new InputStreamReader(
+					new FileInputStream(fileName));
+			BufferedReader bufReader = new BufferedReader(inputReader);
+			String line = "";
+			String Result = "";
+			while ((line = bufReader.readLine()) != null)
+				Result += line;
+
+			System.out.print(Result);
+			return Result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@SuppressLint("NewApi")
+	public static void getURL(String path) {
+		String fileName = "";
+		String dir = "/IndoorNavi/";
+		File sdRoot = Environment.getExternalStorageDirectory();
+		try {
+			// Open the URLConnection for reading
+			URL u = new URL(path);
+			// URL u = new URL("http://www.baidu.com/");
+			HttpURLConnection uc = (HttpURLConnection) u.openConnection();
+
+			int code = uc.getResponseCode();
+			String response = uc.getResponseMessage();
+			System.out.println("HTTP/1.x " + code + " " + response);
+			for (int j = 1;; j++) {
+				String key = uc.getHeaderFieldKey(j);
+				String header = uc.getHeaderField(j);
+				if (!(key == null)) {
+					if (key.equals("Content-Name"))
+						fileName = header;
+				}
+				if (header == null || key == null)
+					break;
+				System.out.println(uc.getHeaderFieldKey(j) + ": " + header);
+			}
+			Log.i("zhr", fileName);
+			System.out.println();
+
+			try (InputStream in = new BufferedInputStream(uc.getInputStream())) {
+
+				// chain the InputStream to a Reader
+				Reader r = new InputStreamReader(in);
+				int c;
+				File mapFile = new File(sdRoot, dir + fileName);
+				mapFile.createNewFile();
+				FileOutputStream filecon = new FileOutputStream(mapFile);
+				while ((c = r.read()) != -1) {
+					System.out.print((char) c);
+					filecon.write(c);
+					filecon.flush();
+
+				}
+				filecon.close();
+			}
+
+		} catch (MalformedURLException ex) {
+			System.err.println(path + " is not a parseable URL");
+		} catch (IOException ex) {
+			System.err.println(ex);
+		}
+	}
+}
---
+++ src/com/example/indoornavi/Scan.java
@@ -0,0 +1,140 @@
+package com.example.indoornavi;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.example.indoornavi.FileHelper;
+import com.jiahuan.svgmapview.SVGMapView;
+import com.jiahuan.svgmapview.SVGMapViewListener;
+import com.jiahuan.svgmapview.overlay.SVGMapLocationOverlay;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.EditText;
+
+public class Scan extends Activity {
+
+	String url = "http://192.168.1.106/api/";
+	EditText et;
+	String filename = "";
+	String dir = "/IndoorNavi/";
+	File sdRoot = Environment.getExternalStorageDirectory();
+	boolean isExist = false;
+	final int file_exist = 1;
+	final int file_no_exist = 2;
+	private SVGMapView mapView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.scan_view);
+		et = new EditText(this);
+		mapView = (SVGMapView) findViewById(R.id.location_mapview);
+
+		mapView.registerMapViewListener(new SVGMapViewListener() {
+			@Override
+			public void onMapLoadComplete() {
+				SVGMapLocationOverlay locationOverlay = new SVGMapLocationOverlay(
+						mapView);
+				locationOverlay.setIndicatorArrowBitmap(BitmapFactory
+						.decodeResource(getResources(),
+								R.drawable.indicator_arrow));
+				locationOverlay.setPosition(new PointF(400, 500));
+				locationOverlay.setIndicatorCircleRotateDegree(90);
+				locationOverlay.setMode(SVGMapLocationOverlay.MODE_COMPASS);
+				locationOverlay.setIndicatorArrowRotateDegree(-45);
+				mapView.getOverLays().add(locationOverlay);
+				mapView.refresh();
+			}
+
+			@Override
+			public void onMapLoadError() {
+			}
+
+			@Override
+			public void onGetCurrentMap(Bitmap bitmap) {
+			}
+		});
+
+		new AlertDialog.Builder(this).setTitle("请输入")
+				.setIcon(android.R.drawable.ic_dialog_info).setView(et)
+				.setPositiveButton("确定", new DialogInterface.OnClickListener() {// 添加确定按钮
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {// 确定按钮的响应事件
+								// TODO Auto-generated method stub
+								filename = et.getText().toString() + ".svg";
+								Log.i("zhr", "locate = " + filename);
+
+								File mkDir = new File(sdRoot, dir);
+								if (!mkDir.exists())
+									mkDir.mkdirs();
+
+								if (mkDir.listFiles().length > 0) {
+									for (File file : mkDir.listFiles()) {
+										String ff = file.getName();
+										if (filename.equals(file.getName())) {
+											isExist = true;
+											break;
+										}
+									}
+									Message message = new Message();
+									if (isExist)
+										message.what = file_exist;
+									else
+										message.what = file_no_exist;
+									mHandler.sendMessage(message);
+								}
+
+							}
+
+						}).setNegativeButton("取消", null).show();
+
+	}
+
+	public Handler mHandler = new Handler() {
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case file_exist:
+				String f = sdRoot.getPath() + dir + filename;
+				Log.i("zhr", f);
+				mapView.loadMap(FileHelper.getContent(f));
+				break;
+
+			case file_no_exist:
+				String url_api = url + "?zip=" + filename.substring(0, 6)
+						+ "&s=" + filename.substring(6, 9);
+				Log.i("zhr", url_api);
+				FileHelper.getURL(url_api);
+				String f2 = sdRoot.getPath() + dir + filename;
+				mapView.loadMap(FileHelper.getContent(f2));
+
+				break;
+			default:
+				break;
+			}
+			super.handleMessage(msg);
+		}
+	};
+
+	
+
+}
